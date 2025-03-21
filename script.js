091c28b9f1d35ff5e99f8bb30ec28e804b5ee0d5
coderman982 <khanfuzail955@gmail.com>
2025-03-21 14:05:02 +0530

    it is hw

--- script.js
+++ script.js
@@ -1,15 +1,86 @@
+const prompt=require("prompt-sync")({sigint:true}); 
 
 
+function compchoice(){
+    let comp=Math.floor(Math.random()*3);
+   
+    if (comp===0) {
+        return "scissor";
 
-for (let i = 1; i <= 100; i++) {
-    if (i % 3 == 0 && i % 5 == 0) {
-      console.log("FizzBuzz");
-    } else if (i % 3 == 0) {
-      console.log("Fizz");
-    } else if (i % 5 == 0) {
-      console.log("Buzz");
-    } else {
-      console.log(i);
+        
     }
-  }
-  
+
+    else if(comp===1){
+        return "rock";
+    }
+
+    else {
+        return "paper";
+    }
+
+
+}
+
+function humanchoice(){
+    let choice=prompt("enter your weapon")
+    return choice;
+}
+let humscore=0;
+let comscore=0;
+
+let playgame=()=>{
+   
+    function play(humanchoice,compchoice){
+
+        if (humanchoice===compchoice) {
+            console.log(`jh${humanchoice}`);
+            
+        }
+
+        else if ((humanchoice==="rock"&&compchoice==="scissor")||
+                 ( humanchoice==="paper"&&compchoice==="rock")||(
+                    humanchoice==="scissor"&&compchoice==="paper")) {
+                        console.log(`human chose${humanchoice}`);
+                        return humscore++;
+                      }
+
+                      else{
+                        console.log(`comp chose${compchoice}`);
+                        return comscore++;
+                        
+                      }
+                    }
+
+
+                      for (let i = 1; i < 5; i++) {
+                        let computer=compchoice();
+                        let human=humanchoice();
+
+                        play(human,computer);
+
+                        
+                      }
+
+
+                      if (humscore===comscore) {
+                        console.log("tue");
+                        
+                      }
+
+                      else if (humscore>comscore) {
+                        console.log("you win")
+                        
+                      }
+
+                      else{
+                        console.log("comp win")
+                      }
+       
+    
+
+    
+
+
+}
+
+playgame();
